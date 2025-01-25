c60bcb8d2748b7e1d3e244ab9028c95a2ed597f1
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-01-25 10:53:35 +0100

    add image on form page

--- src/routes/form/+page.server.js
+++ src/routes/form/+page.server.js
@@ -1,5 +1,9 @@
 export const load = () => {
     return {
-        cica: 1
+        cica: 1,
+        kutya: { name: 'Zozó',
+             age: 3,
+            image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCgNPjogXlDiQcKVYhBfGVDP919tfK8Gc9F8pHxGwUY_75Av1lNyXoCzbzebv026Q_9SbNppHIGqz3UvukEhOiOg' }, // Zozó
+        // kutya_kep: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCgNPjogXlDiQcKVYhBfGVDP919tfK8Gc9F8pHxGwUY_75Av1lNyXoCzbzebv026Q_9SbNppHIGqz3UvukEhOiOg'
     }
 };
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,4 +1,12 @@
 <!-- a + page svelte számít az index fájlnak minden mappában -->
- Zöldség
+<script>
+    let {data} = $props();
+</script> 
+Zöldség
  <h1>Ez lesz a form</h1>
+ <p> Cica: {data.cica}</p>
+<p>Kutya: {data.kutya}</p>
+<p>Kutya: {data.kutya.name}</p>
+<img src="{data.kutya.image}" alt="">
+
 
