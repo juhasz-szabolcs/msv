40683333a506883a86db418fcac52673588996b1
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-01-25 11:55:55 +0100

    szöveg hosszának validálása

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -6,9 +6,17 @@ Zöldség
  <h1>Ez lesz a form</h1>
  <p> Cica: {data.cica}</p>
 
+
+<p>Hány kutyát szeretnél látni? (1,2)</p>
 <form method="post">
-    <input name="x1" bind:value={i1}><br>
-    <input type="submit" value="Küldés">
+    <input 
+        class="{i1 && i1.length > 6 ? 'green' : 'red'}"
+        name="x1" bind:value={i1}><br>
+    <input 
+        disabled={
+        ! (i1 && i1.length > 6)
+        }
+        type="submit" value="Küldés">
 </form>
 
 
@@ -17,4 +25,11 @@ Zöldség
 <img src="{data.kutya_kep}.jpg" alt="" width="200px"><br>
 i1: {i1}
 
-
+<style>
+    .green {
+        background-color: green;
+    }
+    .red {
+        background-color: red;
+    }
+</style>
