d78617d042a28f0ad1a754f4dfe3487b5b0e7dd1
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-01-25 11:17:51 +0100

    send information from frontend

--- src/routes/form/+page.server.js
+++ src/routes/form/+page.server.js
@@ -1,9 +1,20 @@
+var x = 1
 export const load = () => {
     return {
-        cica: 1,
+        cica: x,
         kutya: { name: 'Zozó',
              age: 3,
             image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCgNPjogXlDiQcKVYhBfGVDP919tfK8Gc9F8pHxGwUY_75Av1lNyXoCzbzebv026Q_9SbNppHIGqz3UvukEhOiOg' }, // Zozó
         // kutya_kep: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSCgNPjogXlDiQcKVYhBfGVDP919tfK8Gc9F8pHxGwUY_75Av1lNyXoCzbzebv026Q_9SbNppHIGqz3UvukEhOiOg'
     }
 };
+
+export const actions = {
+    default: async ({ locals, request }) => {  
+        console.log(locals)
+        const data = await request.formData();
+        const x1 = data.get('x1')
+        console.log(x1)
+        x++
+    }
+}
--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -5,8 +5,16 @@
 Zöldség
  <h1>Ez lesz a form</h1>
  <p> Cica: {data.cica}</p>
+
+<form method="post">
+    <input name="x1"><br>
+    <input type="submit" value="Küldés">
+</form>
+
+
 <p>Kutya: {data.kutya}</p>
 <p>Kutya: {data.kutya.name}</p>
 <img src="{data.kutya.image}" alt="">
+<img src="./1.jpg" alt="">
 
 
